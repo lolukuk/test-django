5b1a2e55ae40aac3bde232f615301a2042dc6913
lolukuk <demogs1@gmail.com>
2024-07-27 21:57:06 +0700

    update urls.py, views.py, menu_tags draw_menu and base

--- menu_project/menu/templatetags/menu_tags.py
+++ menu_project/menu/templatetags/menu_tags.py
@@ -1,16 +1,14 @@
 from django import template
-from menu.models import Menu, MenuItem  # Исправление импорта
+from menu.models import Menu, MenuItem
+from django.core.exceptions import ObjectDoesNotExist
 
 register = template.Library()
 
-@register.inclusion_tag('menu/draw_menu.html', takes_context=True)
-def draw_menu(context, menu_name):
-    # Логика для получения меню по имени
-    request = context['request']
-    menu = Menu.objects.get(name=menu_name)
-    menu_items = MenuItem.objects.filter(menu=menu)
-    return {
-        'menu': menu,
-        'menu_items': menu_items,
-        'current_path': request.path,
-    }
+@register.inclusion_tag('menu/draw_menu.html')
+def draw_menu(menu_name):
+    try:
+        menu = Menu.objects.get(name=menu_name)
+        menu_items = MenuItem.objects.filter(menu=menu, parent=None)
+        return {'menu_items': menu_items}
+    except ObjectDoesNotExist:
+        return {'menu_items': []}
--- menu_project/menu/views.py
+++ menu_project/menu/views.py
@@ -1,22 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
-from django.template.loader import render_to_string
-from django.template.defaultfilters import slugify
+from django.shortcuts import render
 
-from .models import MenuItem
-
-menu = [{'title': "О сайте", 'url_name': 'about'}]
-
-# Create your views here.
 def index(request):
-    menu = MenuItem
-
-    data = {
-        'title': 'Главная страница',
-        'menu': 'menu'
-    }
-    return render(request, 'menu/draw_menu.html', context=data)
-
-def about(request):
-    return render(request, 'base.html', {'title': 'О сайте', 'menu': menu})
+    return render(request, 'base.html')
