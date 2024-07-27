41e5ddd12586aecdfb4efe0dd901fa4ad2689210
lolukuk <demogs1@gmail.com>
2024-07-27 21:22:24 +0700

    update template

--- menu_project/menu/admin.py
+++ menu_project/menu/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Menu, MenuItem
 
-# Register your models here.
+class MenuItemInline(admin.StackedInline):
+    model = MenuItem
+    extra = 1
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    inlines = [MenuItemInline]
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'menu', 'parent', 'order']
+    list_filter = ['menu']
